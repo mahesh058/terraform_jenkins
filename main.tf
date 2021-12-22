08a2b7a2de0f8773ad63dce06456aa7d2df9f386
mahesh058 <86655799+mahesh058@users.noreply.github.com>
2021-12-21 20:24:22 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
 provider "aws" { 
   region     = "ap-south-1"
-  access_key = var.access
-  secret_key = var.secret
 }
 
 
-variable "env" {
+/*variable "env" {
     default = "prod"
-}
+}*/
 
 
 resource "aws_s3_bucket" "demo-mcd058" {
-    count = "${var.env == "prod" ? 1 : 0}"
+    #count = "${var.env == "prod" ? 1 : 0}"
     bucket = "demo-mcd-058"
 }
