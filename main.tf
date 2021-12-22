c3c07552b58140c6b89a56e1811b5cd37c06998f
mahesh058 <86655799+mahesh058@users.noreply.github.com>
2021-12-22 02:34:38 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ variable "env" {
 
 
 resource "aws_s3_bucket" "demo-mcd058" {
-    count = "${var.env == "true"?1:0}"
+    count = "${var.env == "'true'"?1:0}"
     bucket = "demo-mcd-58"
 }
 
 resource "aws_instance" "MCDpoc"{
-    count = "${var.env == "false" ? 1: 0}"
+    count = "${var.env == "'false'" ? 1: 0}"
     ami = "ami-0b9064170e32bde34"
     instance_type = "t2.micro" 
 }
